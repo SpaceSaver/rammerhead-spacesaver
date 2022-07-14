9223fd18cb2dd2c0d1ea68350e6bdbf97a3a26d7
SpaceSaver <60277022+SpaceSaver@users.noreply.github.com>
2022-07-14 11:47:26 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -43,7 +43,7 @@ module.exports = {
     //     // 'x-frame-options': (originalHeaderValue) => '',
     //     'x-frame-options': null, // set to null to tell rammerhead that you want to delete it
     // },
-    rewriteServerHeaders: {},
+    rewriteServerHeaders: {'access-control-allow-origin': '*'},
 
     //// SESSION STORE CONFIG ////
 
